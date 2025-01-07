8f89d6a69ee5edd0c06ec2b6bad0a000f227648d
hafij <hafij.alasad23@gmail.com>
2025-01-08 00:09:45 +0600

    feat: mysql database connected and data get operation initialized

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     function getUser(){
-        return "Hafij Al Asad";
+        return DB::select("select * from users");
     }
 
     function getUserName($name){
