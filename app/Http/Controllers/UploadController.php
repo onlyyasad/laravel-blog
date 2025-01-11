998cb5e3f499ab6b4df95f1a6a5415bbc700233b
hafij <hafij.alasad23@gmail.com>
2025-01-12 00:41:26 +0600

    feat: uploaded file linked to print in ui and printed

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -8,6 +8,10 @@ class UploadController extends Controller
 {
     //
     function upload(Request $req){
-        return $req->file('file')->store('public');
+        // Store file explicitly in the 'public' disk
+        $file = $req->file('file')->store('public', ['disk' => 'public']);
+        $fileName = basename($file); // Get the file name
+
+        return view('display', ['image' => $fileName]);
     }
 }
---
+++ resources/views/display.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>Uploaded Image</h1>
+    <img style="width: 300" src="{{url('storage/public/'.$image)}}" alt="Image"/>
+</div>
