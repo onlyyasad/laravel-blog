78ea693bc1f9780652dc232814f70483c3acbca5
hafij <hafij.alasad23@gmail.com>
2025-01-19 23:19:28 +0600

    feat: navbar added from bootstrap library

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,11 +8,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body class="mx-auto p-6">
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/student/list">Students</a>
-    </nav>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Laravel</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/student/list">Students</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     <div>
         {{$main}}
     </div>
