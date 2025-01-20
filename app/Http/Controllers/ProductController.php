8efbb6c761ea7d35b8c5d6f78680fd81f94749a8
hafij <hafij.alasad23@gmail.com>
2025-01-20 23:42:37 +0600

    feat: seller migration, model, controller , and products migration, model, controller created

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return "write some code";
+    }
+
+}
