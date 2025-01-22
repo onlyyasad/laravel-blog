2b309eb21a2f1bf769aa7d5073ddb5a3fd5d871c
hafij <hafij.alasad23@gmail.com>
2025-01-22 22:40:54 +0600

    refactor: update student form polished and improved ui with bootstrap

--- resources/views/edit-student.blade.php
+++ resources/views/edit-student.blade.php
@@ -5,33 +5,34 @@
     <x-slot name="main">
         <div>
             <h1>Update Student</h1>
-            <form action="/student/edit-student/{{$student->id}}" method="POST">
+            <form action="/student/edit-student/{{$student->id}}" method="POST" class="row g-3">
                 @csrf
                 <input type="hidden" name="_method" value="PUT">
-                <div>
-                    <label for="name">Name</label>
-                    <input type="text" name="name" value="{{$student->name}}" id="name">
+                <div class="col-12">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" name="name" value="{{$student->name}}" id="name" class="form-control">
                 </div>
-                <br>
-                <div>
-                    <label for="email">Email</label>
-                    <input type="email" name="email" value="{{$student->email}}" id="email">
+                
+                <div class="col-12">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" value="{{$student->email}}" id="email" class="form-control">
                 </div>
-                <br>
-                <div>
-                    <label for="phone">Phone</label>
-                    <input type="text" name="phone" value="{{$student->phone}}" id="phone">
+                
+                <div class="col-12">
+                    <label for="phone" class="form-label">Phone</label>
+                    <input type="text" name="phone" value="{{$student->phone}}" id="phone" class="form-control">
                 </div>
-                <br>
-                <div>
-                    <label for="batch">Batch</label>
-                    <input type="text" name="batch" value="{{$student->batch}}" id="batch">
+                
+                <div class="col-12">
+                    <label for="batch" class="form-label">Batch</label>
+                    <input type="text" name="batch" value="{{$student->batch}}" id="batch" class="form-control">
                 </div>
-                <br>
-                <div>
-                    <button type="submit">Update Student</button>
-                    <a href="student/list"><button type="button">Cancel</button></a>
+                
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Update Student</button>
+                    <a href="student/list"><button type="button" class="btn btn-danger">Cancel</button></a>
                 </div>
+            </form>
         </div>
         
     </x-slot>
