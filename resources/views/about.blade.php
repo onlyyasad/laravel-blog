c3adb7c699e66b44cd014313cc9edab93c2ebf93
hafij <hafij.alasad23@gmail.com>
2025-01-23 23:33:18 +0600

    feat: lorem ipsum text added in about section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,6 +5,6 @@
     <x-slot name="main">
         <h1>About</h1>
         <p>This is a simple website created with Laravel.</p>
-        <p>It is a project for the Laravel course at Codecademy.</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </x-slot>
 </x-layout>
